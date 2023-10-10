b3f3348a154d55c2101cefa248adf527052d4be0
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-10-09 19:01:49 -0500

    Added Get the Fish to header links

--- conf.py
+++ conf.py
@@ -150,6 +150,7 @@ NAVIGATION_LINKS = {
                 ("https://necromuralist.github.io/", "The Cloistered Monkey"),
                 ("https://necromuralist.github.io/Ape-Iron/", "Ape Iron"),
                 ("https://necromuralist.github.io/Bowling-For-Data/", "Bowling For Data"),
+                ("https://necromuralist.github.io/Give-The-Fish/", "Give the Fish"),
                 ("https://necromuralist.github.io/Neurotic-Networking/", "Neurotic Networking"),
                 ("https://necromuralist.github.io/Visions-Voices-Data/", "Visions, Voices, Data")
                 ),
